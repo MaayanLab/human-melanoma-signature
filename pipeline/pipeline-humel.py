240b2a09178460c428edb58cb20e85a74137c781
Ma'ayan <maayanlab@maayans-pro-2.wireless.mountsinai.org>
2018-07-17 11:10:37 -0400

    generate count matrix from ARCHS4

--- pipeline/pipeline-humel.py
+++ pipeline/pipeline-humel.py
@@ -1,6 +1,6 @@
 #################################################################
 #################################################################
-############### ARCHS4 Data Packaging ################
+############### Melanoma Signature Profile ################
 #################################################################
 #################################################################
 ##### Author: Julia Zhao
@@ -12,34 +12,90 @@
 #############################################
 ##### 1. Python modules #####
 from ruffus import *
-import sys, os, json
+import sys, os, json, operator#, geode, RNAseq, operator, sklearn.metrics.pairwise
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import functools
 
 ##### 2. Custom modules #####
 # Pipeline running
+sys.path.append('pipeline/scripts')
+from Melanoma import *
 
 #############################################
 ########## 2. General Setup
 #############################################
 ##### 1. Variables #####
+archs_matrix = 'rawdata/human_matrix.h5'
+metadata_file = 'rawdata/mela_rna_3rep_short.csv'
 
-
 #######################################################
 #######################################################
-########## S1. 
+########## S1. Find signatures of each melanoma study
 #######################################################
 #######################################################
+# We process the ARCHS4 human gene expression data, to exact RNA sequencing data of the samples of interest
 
 #############################################
-########## 1. 
+########## 1. Read data from ARCHS4
 #############################################
 
-@files(None, None)
-def test(infile, outfile):
-	print('test')
+# Make a directory that will host all runned results
+@mkdir('s1-expression_data.dir')
+# Merge two starting files as a 1 list, and export this single output to the result folder
+@merge([archs_matrix, metadata_file],
+	   's1-expression_data.dir/melanoma-counts.txt')
+
+
+def getCounts(infiles, outfile):
+		# Read infiles, create a list of gsms to be analyzed
+	archs_matrix, metadata_file = infiles
+	# read metadata file in to pandas
+	metadata_df = pd.read_csv(metadata_file)
+	# Save all ctrl_ids to a new dataframe, as lists of lists
+	ctrlgsm = np.array(metadata_df['ctrl_id'].str.strip().str.split())
+	# Save all perturb_ids to a new dataframe, as lists of lists
+	perturbgsm = np.array(metadata_df['perturb_id'].str.strip().str.split())
+	gsmlist=np.concatenate((ctrlgsm, perturbgsm))
+	# Reduce lists of lists to 1 long list of strings
+	gsmlist = functools.reduce (operator.concat, gsmlist)
+	# Read expression data
+	expression_dataframe = load_read_counts(archs_matrix, gsms = gsmlist)
+	# Save to outfile
+	expression_dataframe.to_csv(outfile, sep='\t')
 
 #############################################
-########## 2. 
+########## 2. Normalize the gene expression table
+#############################################
+##### Use the method "count per million" to normalize gene expression
+# @transform(expression_dataframe,suffix('-counts.txt'),'_cpm.csv')
+
+# def getCPM (infile, ourfile):
+# 	cpm_table = compute_CPMs (infile)
+# 	cpm_table.to_csv(outfile, sep="\t")
+
+##################################################
+##################################################
+########## Run pipeline
+##################################################
+##################################################
+
+#############################################
+########## 2. Normalize the gene expression table
+#############################################
+##### Describe the step
+### Input: 
+### Output: 
+
+##################################################
+##################################################
+########## Run pipeline
+##################################################
+##################################################
+
+#############################################
+########## 2. Normalize the gene expression table
 #############################################
 ##### Describe the step
 ### Input: 
---
+++ pipeline/scripts/Melanoma.py
@@ -0,0 +1,82 @@
+#################################################################
+#################################################################
+############### Melanoma Analysis - Python Support ############
+#################################################################
+#################################################################
+##### Author: Julia Zhao
+##### Affiliation: Ma'ayan Laboratory,
+##### Icahn School of Medicine at Mount Sinai
+
+#############################################
+########## 1. Load libraries
+#############################################
+##### 1. Python modules #####
+import h5py
+import numpy as np
+import pandas as pd
+
+##### 2. Custom modules #####
+# Pipeline running
+
+
+#############################################
+########## 2. General Setup
+#############################################
+##### 1. Variables #####
+
+#######################################################
+#######################################################
+########## S1. Zichen's supporting functions
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Load read counts
+#############################################
+
+def load_read_counts(h5_file, gsms=[]):
+	'''Load data from h5 file using a list of GSMs.
+	'''
+	f = h5py.File(h5_file, 'r')
+	mat = f['data']['expression']
+
+	gsms = [str.encode(x) for x in gsms]
+
+	all_gsms = f['meta']['Sample_geo_accession']
+
+	sample_mask = np.in1d(all_gsms, gsms)
+	if sample_mask.sum() == 0:
+		raise RuntimeError('None of the gsms exists in the h5 file')
+	else:
+		sample_ids = all_gsms[sample_mask]
+		genes = f['meta']['genes']
+		# to prevent MongoDB error
+		genes = map(lambda x:x.decode('utf-8').replace('.', '_'), genes)		
+		# Retrieve gene by sample matrixpytho
+		expr_df = pd.DataFrame(mat[sample_mask, :].T, index=genes, columns=sample_ids)
+
+		# Filter out non-expressed genes
+		expr_df = expr_df.loc[expr_df.sum(axis=1) > 0, :] 
+
+		# Fix column names
+		expr_df.columns = [x.decode('utf-8') for x in expr_df.columns]
+		# Filter out samples with very low read counts
+		# valid_sample_mask = expr_df.sum(axis=0) > 100
+		# expr_df = expr_df.loc[:, valid_sample_mask]
+
+		return expr_df
+		
+#############################################
+########## 2. Compute CPMs
+############################################# 
+def compute_CPMs(expr_df, CPM_cutoff=0.3, at_least_in_persent_samples=10):
+	'''Convert expression counts to CPM.
+	'''
+	n_samples = expr_df.shape[1]
+	at_least_in_n_samples = int(math.ceil(at_least_in_persent_samples/100. * n_samples))
+
+	expr_df = (expr_df * 1e6) / expr_df.sum(axis=0)
+	# Filter out lowly expressed genes
+	mask_low_vals = (expr_df > CPM_cutoff).sum(axis=1) > at_least_in_n_samples
+	expr_df = expr_df.loc[mask_low_vals, :]
+	return expr_df
